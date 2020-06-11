08e5c497f42a49d478cf375ea56787d84ebc6338
dipakchhajed <60436710+dipakchhajed@users.noreply.github.com>
2020-06-11 09:21:11 +0530

    Update API_SQL Project.sql
    Hi praveen,
    i have remove the comment kindly proceed to validate.
    Regards,
    dipak

--- API_SQL Project.sql
+++ API_SQL Project.sql
@@ -1,31 +1,6 @@
-/* Welcome to the SQL mini project. For this project, you will use
-Springboard' online SQL platform, which you can log into through the
-following link:
-
-https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
-
-The data you need is in the "country_club" database. This database
-contains 3 tables:
-    i) the "Bookings" table,
-    ii) the "Facilities" table, and
-    iii) the "Members" table.
-
-Note that, if you need to, you can also download these tables locally.
-
-In the mini project, you'll be asked a series of questions. You can
-solve them using the platform, but for the final deliverable,
-paste the code for each solution into this script, and upload it
-to your GitHub.
-
-Before starting with the questions, feel free to take your time,
-exploring the data, and getting acquainted with the 3 tables. */
-
-
-
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
+
 select  distinct(Mb.firstname + ' ' +Mb.surname) As MemberName,Fac.membercost As MemberCost from dbo.Members Mb
 left join Bookings Bk on Mb.memid = Bk.memid
 left join Facilities Fac on Bk.facid = Fac.facid
@@ -33,6 +8,7 @@ where Fac.membercost > 0
 
 
 /* Q2: How many facilities do not charge a fee to members? */
+
 select  count(distinct(Mb.firstname +' '+ Mb.surname) ) As Membercount 
 from dbo.Members Mb
 left join Bookings Bk on Mb.memid = Bk.memid
@@ -40,6 +16,7 @@ left join Facilities Fac on Bk.facid = Fac.facid
 where Fac.membercost = 0
 
 /* List of facilities do not charge a fee to members */ 
+
 select  distinct(Mb.firstname + ' ' +Mb.surname) As MemberName,Fac.membercost As MemberCost from dbo.Members Mb
 left join Bookings Bk on Mb.memid = Bk.memid
 left join Facilities Fac on Bk.facid = Fac.facid
@@ -49,6 +26,7 @@ where Fac.membercost = 0
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
+
 select  facid, Name , membercost,monthlymaintenance  
 from dbo.Facilities where membercost < (monthlymaintenance/100)*20
 
@@ -73,6 +51,7 @@ select  firstname +' '+ surname from dbo.Members where  joindate = (select max(j
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
 select  distinct(Mb.firstname +' '+ Mb.surname), Fac.name As FacilityName
 from dbo.Members Mb
 left join Bookings Bk on Mb.memid = Bk.memid
@@ -86,6 +65,7 @@ different costs to members (the listed costs are per half-hour 'slot'), and
 the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
+
 select  distinct(Mb.firstname +' '+ Mb.surname) As MemberName, Fac.name As FacilityName,Fac.membercost+Fac.guestcost As TotCost
 from dbo.Members Mb
 left join Bookings Bk on Mb.memid = Bk.memid
@@ -94,6 +74,7 @@ where Bk.starttime like '2012-09-14%' and Fac.membercost+Fac.guestcost >30
 order by Mb.firstname +' '+ Mb.surname
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
 select  distinct(Mb.firstname +' '+ Mb.surname) As MemberName, Fac.name As FacilityName,Fac.membercost+Fac.guestcost As TotCost
 from dbo.Members Mb,Bookings Bk,Facilities Fac
 where Mb.memid = Bk.memid
@@ -105,6 +86,7 @@ order by Mb.firstname +' '+ Mb.surname
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
+
 select (membercost+guestcost+initialoutlay+monthlymaintenance) TotRev,name 
 from Facilities 
 where (membercost+guestcost+initialoutlay+monthlymaintenance) >1000
